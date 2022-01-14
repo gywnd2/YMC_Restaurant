c4210dd2061db2355544142b6682501e20c01a14
Hyo-jung Lee <leeexpert@cau.ac.kr>
2022-01-14 22:55:15 +0900

    템플릿 적용하기

---
+++ demo/src/main/java/com/udangtangtang/restaurant/YmcRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.udangtangtang.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmcRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmcRestaurantApplication.class, args);
+	}
+
+}
